72fee3b0832d651ce94383ba37545a51654c414f
besalelu <besalelu.php@gmail.com>
2022-02-23 02:58:54 +0530

    first commit

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts';
+
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+ 
+const MasterLayout = () => {
+  return (
+    <div className="sb-nav-fixed">
+          <Navbar />
+          <div id="layoutSidenav">
+              <div id="layoutSidenav_nav">
+                  <Sidebar />
+              </div>
+              <div id="layoutSidenav_content">
+                  <main>
+                    Master File Content
+                  </main>
+                  <Footer />
+              </div>
+          </div>
+    </div>
+  );
+}
+
+export default MasterLayout;
